43f130698763cc7f8311f9c37ea55515a277af9b
Martinusbe <martinusbe@gmail.com>
2017-10-15 23:59:18 +0200

    envsetup: correct path to repopick in vendor
     * needs a total overhaul
    
    Change-Id: I2636d8028ffb0c2680dbfa9ec8f8bc80e13a5945

--- envsetup.sh
+++ envsetup.sh
@@ -1604,7 +1604,7 @@ function reposync() {
 
 function repopick() {
     T=$(gettop)
-    $T/build/tools/repopick.py $@
+    $T/vendor/aosp/build/tools/repopick.py $@
 }
 
 function fixup_common_out_dir() {
